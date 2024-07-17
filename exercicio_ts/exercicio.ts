292d82b2f115b4fe362cce18bde77d4d5a704d30
rafaelkamada1 <150379665+rafaelkamada1@users.noreply.github.com>
2024-07-17 15:44:59 -0300

    Add files via upload

---
+++ exercicio_ts/exercicio.ts
@@ -0,0 +1,12 @@
+function multiplicacao(num1: number, num2: number): number {
+    return num1 * num2;
+}
+
+console.log(multiplicacao(5, 3));  
+
+function saudacao(nome: string): string {
+    return "Olá " + nome;
+}
+
+console.log(saudacao("João"));  
+
